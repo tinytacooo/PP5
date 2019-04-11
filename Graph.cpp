de52dad3c41586f2164e85b1392504aab0f292f2
tinytacooo <39066347+tinytacooo@users.noreply.github.com>
2019-04-11 10:43:45 -0400

    lots of stuff
     - greatly improved Vertex and Edge housekeeping functions
        - corrected storing uneeded references
        - corrected deleting objects that were still in use
     - removed unecessary methods
     - added comments
     - deleted print method

--- Graph.cpp
+++ Graph.cpp
@@ -3,51 +3,45 @@
 /* GRAPH methods */
 
 void Graph::addVertex(std::string label) {
-    V.insert({label, Vertex(label)});
+    V.insert({label, Vertex(label)});       // add new Vertex object (and its label) to the vector map in Graph
 }
 
 void Graph::removeVertex(std::string label) {
-    for (std::map<int, Edge>::iterator it = E.begin(); it != E.end(); it++)
-        removeEdge(label);
-
-    for (std::map<std::string, Vertex>::iterator it = V.begin(); it != V.end(); it++)
-        it->second.removeAdjVertex(label);
-
-    V.erase(label);
+    removeEdge(label);          // remove edges associated with this vertex
+    removeAdjacencies(label);   // remove references to this vertex from adjacency list in other vertices
+    V.erase(label);             // erase Vertex entry in map
 }
 
 void Graph::addEdge(std::string label1, std::string label2, unsigned long weight) {
     if(getEdgeId(label1, label2) == 0) {
-        E.insert({++edgeId, Edge(label1, label2, weight)});
+        E.insert({++edgeId, Edge(label1, label2, weight)});     // add unique edgeId and Edge to map in Graph
 
-        addVertex(label1);
-        addVertex(label2);
+        addVertex(label1);              // if needed, add starting vertex to Vector map
+        addVertex(label2);              // if needed, add ending vertex to Vector map
 
-        addAdjacency(label1, label2);
-        addAdjacency(label2, label1);
+        addAdjacency(label1, label2);   // add reference to end vertex to start vertex's adjacency list
+        addAdjacency(label2, label1);   // (and vice versa)
     }
 }
 
 void Graph::removeEdge(std::string label1, std::string label2) {
-    int id = getEdgeId(label1, label2);
-    if (id != 0) {
-        removeVertex(label1);
-        removeVertex(label2);
-        E.erase(id);
+    int id = getEdgeId(label1, label2);           // find unique edgeId associated with this vertex pair
+    if (id != 0) {                                // if there is a nonzero edgeId for the pair
+        removeAdjacencies(label1, label2);        // remove references to each vertex from the other's adjacency list
+        E.erase(id);                              // erase entry in Edge map
     }
 }
 
-void Graph::removeEdge(std::string label1) {
-    if (getEdgeId(label1) != 0)
-        E.erase(getEdgeId(label1));
+void Graph::removeEdge(std::string label) {
+    for (std::map<int, Edge>::iterator it = E.begin(); it != E.end(); it++)
+        removeEdge(label, it->second.getOtherVertex(label));               // remove any edges containing the specified vertex
 }
 
 unsigned long Graph::shortestPath(std::string startLabel, std::string endLabel, std::vector<std::string> &path) {
     std::map<std::string, unsigned long> unvisited;    // contains unvisited vertices: <label, distance_to_vertex>
     std::map<std::string, std::string> visited;        // contains visited vertices:   <label, adjacent_vertex_with_shortest_distance>
-    unsigned long shortestDistance = 0;                // return this value as the distance of the shortest possible path
 
-    // set all vertices in "unvisited" to have an initial distance of 'infinity' from the start vtx (except start vtx, which has an initial distance of 0)
+    // set all vertices in "unvisited" to have an initial distance of 'infinity' from the start vertex (except start vertex, which has an initial distance of 0)
     // set initial shortest path to each vertex as itself (eventually replace these values using Djikstra's algorithm)
     for (auto it : V) {
         (it.first == startLabel) ? unvisited.insert({it.first, 0}) : unvisited.insert({it.first, LONG_MAX});
@@ -59,63 +53,45 @@ unsigned long Graph::shortestPath(std::string startLabel, std::string endLabel, 
         std::string currLabel = getMinDistanceLabel(unvisited);     // get label of next vertex w/ shortest distance to start vertex
         unsigned long currDistance = unvisited.at(currLabel);       // get distance associated with current vertex
 
-        for (auto it : V.at(currLabel).getAdjacentVertices()) {             // iterate through list of vertices adjacent to the current vertex
-            if (isUnvisited(it.first, unvisited)) {                         // if adjacent vertex is unvisited
-                // temporarily store value of distance associated with the adjecent vertex
-                unsigned long oldDistance = unvisited.at(it.first);
-                // compare the adjecent vertex's distance (oldDistance) with potential new distance (current vertex distance + weight of edge (curr, adj))
-                unvisited.at(it.first) = compareDistances(currLabel, it.first, currDistance, oldDistance);
-                // if oldDistance was replaced with newDistance, update shortest path to adjacent vertex with current vertex
-                if (oldDistance != unvisited.at(it.first)) { visited.at(it.first) = currLabel; }
+        for (auto it : V.at(currLabel).getAdjacentVertices()) {                                             // iterate through list of vertices adjacent to the current vertex
+            if (isUnvisited(it.first, unvisited)) {                                                         // if adjacent vertex is unvisited:
+                unsigned long oldDistance = unvisited.at(it.first);                                         // temporarily store value of distance associated with the adjacent vertex
+                unvisited.at(it.first) = shorterDistance(currLabel, it.first, currDistance, oldDistance);   // compare the adjacent vertex's distance (oldDistance) with potential new distance (current_vertex_distance + weight_of_edge(curr, adj))
+                if (oldDistance != unvisited.at(it.first)) { visited.at(it.first) = currLabel; }            // if oldDistance was replaced with newDistance (ie newDistance is shorter), update shortest path to adjacent vertex with current vertex
             }
         }
 
-        unvisited.erase(currLabel);         // remove vertex from "unvisited" list
+        unvisited.erase(currLabel);                             // remove (now visited) vertex from "unvisited" list
     }
 
-    shortestDistance = buildPath(startLabel, endLabel, visited, path);
-
-    return shortestDistance;
+    return buildPath(startLabel, endLabel, visited, path);      // build the path and return the shortest distance calculated
 }
 
+/* billy cobham - glass menagerie */
+
 /* GRAPH helpers */
+
 void Graph::addAdjacency(std::string curr, std::string adj) {
     V.at(curr).addAdjVertex(adj, &(V.at(adj)));
 }
 
 unsigned long Graph::buildPath(std::string startLabel, std::string endLabel, std::map<std::string, std::string> visited, std::vector<std::string>& path) {
     std::string currLabel = endLabel;
-    unsigned long sum = 0;
+    unsigned long sum = 0;                  // shortest path; return this number
+    path.emplace(path.begin(), endLabel);   // add ending vertex to the beginning of the path
 
-    path.emplace(path.begin(), endLabel);
     while (currLabel != startLabel) {
-        path.emplace(path.begin(), visited.at(currLabel));
-        sum += getWeight(currLabel, visited.at(currLabel));
-        currLabel = visited.at(currLabel);
+        path.emplace(path.begin(), visited.at(currLabel));      // add current vertex to beginning of path
+        sum += getWeight(currLabel, visited.at(currLabel));     // add edge weight bewteen current and next vertex to the shortest path
+        currLabel = visited.at(currLabel);                      // go to next vertex in the path
     }
 
     return sum;
 }
 
-unsigned long Graph::compareDistances(std:: string curr, std::string adj, unsigned long currDistance, unsigned long oldDistance) {
-    unsigned long newDistance = currDistance + getWeight(curr, adj);
-    if (newDistance < oldDistance)
-        return newDistance;
-    else
-        return oldDistance;
-}
-
 int Graph::getEdgeId(std::string label1, std::string label2) {
     for (auto it : E) {
         if (it.second.isStartVertex(label1, label2) && it.second.isEndVertex(label1, label2))
-            return it.first;
-    }
-    return 0;
-}
-
-int Graph::getEdgeId(std::string label1) {
-    for (auto it : E) {
-        if (it.second.isStartVertex(label1) || it.second.isEndVertex(label1))
             return it.first;
     }
     return 0;
@@ -141,19 +117,28 @@ bool Graph::isUnvisited(std::string label, std::map<std::string, unsigned long> 
     return (unvisited.find(label) != unvisited.end());
 }
 
-/* REMOVE */
+void Graph::removeAdjacencies(std::string label) {
+    for (std::map<std::string, Vertex>::iterator it = V.begin(); it != V.end(); it++)
+        it->second.removeAdjacency(label);
+}
 
-void Graph::printGraph() {
-    std::cout << "VERTICES:\n";
-    for (auto it : V) {
-        std::cout << it.second.getLabel() << "  =>";
-        for (auto itr : it.second.getAdjacentVertices())
-            std::cout << "  " << itr.first;
-        std::cout << "\n";
-    }
+void Graph::removeAdjacencies(std::string label1, std::string label2) {
+    V.at(label1).removeAdjacency(label2);
+    V.at(label2).removeAdjacency(label1);
+}
 
-    std::cout << "EDGES:\n";
-    for (auto it : E) {
-        std::cout << it.first << ":  " << it.second.getStartVertex() <<  "  " << it.second.getEndVertex() << "  " << it.second.getWeight() << std::endl;
-    }
+unsigned long Graph::shorterDistance(std:: string curr, std::string adj, unsigned long currDistance, unsigned long oldDistance) {
+    unsigned long newDistance = currDistance + getWeight(curr, adj);
+    return ((newDistance < oldDistance) ? newDistance : oldDistance);
+}
+
+/* EDGE methods */
+
+std::string Edge::getOtherVertex(std::string label) {
+    if (isStartVertex(label))
+        return endVertex;
+    else if (isEndVertex(label))
+        return startVertex;
+    else
+        return "";
 }
--- Graph.hpp
+++ Graph.hpp
@@ -5,7 +5,6 @@
 #include <algorithm>
 #include <climits>
 #include <map>
-#include <iostream> // REMOVE
 
 class Vertex {
 public:
@@ -15,9 +14,9 @@ public:
     /* functions */
     Vertex(std::string l) : label(l) {};
     void addAdjVertex(std::string l, Vertex* v) { adjVertices.insert({l, v}); }
-    void removeAdjVertex(std::string label) { adjVertices.erase(label); }
     std::map<std::string, Vertex*> getAdjacentVertices() const { return adjVertices; }
     std::string getLabel() const { return label; }
+    void removeAdjacency(std::string label) { adjVertices.erase(label); }
 };
 
 class Edge {
@@ -32,6 +31,7 @@ public:
     std::string getEndVertex() { return endVertex; }
     unsigned long getWeight() { return weight; }
     /* helpers */
+    std::string getOtherVertex(std::string label);
     bool isStartVertex(std::string l1, std::string l2) { return (startVertex == l1 || startVertex == l2); }
     bool isStartVertex(std::string l1) { return (startVertex == l1); }
     bool isEndVertex(std::string l1, std::string l2) { return (endVertex == l1 || endVertex == l2); }
@@ -49,18 +49,18 @@ public:
     void removeVertex(std::string label);
     void addEdge(std::string label1, std::string label2, unsigned long weight);
     void removeEdge(std::string label1, std::string label2);
-    void removeEdge(std::string label1);
     unsigned long shortestPath(std::string startLabel, std::string endLabel, std::vector<std::string> &path);
     /* helpers */
     void addAdjacency(std::string curr, std::string adj);                           // add vertex adjaceny
     unsigned long buildPath(std::string startLabel, std::string endLabel, std::map<std::string, std::string> visited, std::vector<std::string>& path);
-    unsigned long compareDistances(std:: string curr, std::string adj, unsigned long currDistance, unsigned long oldDistance);
     int getEdgeId(std::string label1, std::string label2);                          // return edgeId of edge containing a pair of vertices
-    int getEdgeId(std::string label1);                                              // return edgeId of edge containing a vertex
     std::string getMinDistanceLabel(std::map<std::string, unsigned long> vDist);    // return label of vertex w/ min distance to start vertex
     unsigned long getWeight(std::string curr, std::string adj);                     // get weight of edge, given a pair of vertices
     bool isUnvisited(std::string label,std:: map<std::string, unsigned long> unvisited);
-    void printGraph(); // REMOVE
+    void removeAdjacencies(std::string label);
+    void removeAdjacencies(std::string label1, std::string label2);
+    void removeEdge(std::string label);
+    unsigned long shorterDistance(std:: string curr, std::string adj, unsigned long currDistance, unsigned long oldDistance);
 };
 
 
